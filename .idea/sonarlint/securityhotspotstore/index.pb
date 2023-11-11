c860c612d5e7e641a7ccd872f13bf5d3ac6d0737
Craig Abt Tomkow <ctomkow@gmail.com>
2023-11-11 12:33:36 -0700

    proto

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,9 @@
 
 @
-traffic_light.py,f/b/fb17d23ded3f311cc10b936b6b92eb89a8e7d2c9
+traffic_light.py,f\b\fb17d23ded3f311cc10b936b6b92eb89a8e7d2c9
+>
+pyproject.toml,5\d\5d07e7d72637aa0d59c89d381fe6dc4cf46e2491
+U
+%.github/workflows/release-package.yml,2\1\21464797e2a2877b680776108d9f930290302714
+L
+src/traffic_light/version.py,b\6\b65a07893d5cd3a42e0475bae410b2ba866bfa0b
--- traffic_light.py
+++ traffic_light.py
@@ -1,62 +1,39 @@
 #!/usr/bin/env python3
 
-import time
-from datetime import datetime
-from zoneinfo import ZoneInfo
-import pprint
-
 import gpiod
 from gpiod.line import Direction, Value
-import requests
-import msal
+from fastapi import FastAPI
 
 LINE_RED = 9
 LINE_YLW = 10
 LINE_GRN = 11
 
-def acquire_token():
-    """
-    Acquire token via MSAL
-    """
-    authority_url = 'https://login.microsoftonline.com/{tenant_id_or_name}'
-    app = msal.ConfidentialClientApplication(
-        authority=authority_url,
-        client_id='{client_id}',
-        client_credential='{client_secret}'
-    )
-    token = app.acquire_token_for_client(scopes=["https://graph.microsoft.com/.default"])
-    return token
+app = FastAPI()
 
-# calendar
-cal_details = {
-    "Schedules": ["craig.tomkow@bc.net"],
-    "StartTime": {
-        "dateTime": f'{datetime.now(tz=ZoneInfo("America/Edmonton")).replace(microsecond=0,second=0,minute=0,hour=9).isoformat()[:-6]}',
-        "timeZone": "Mountain Standard Time"
-    },
-    "EndTime": {
-        "dateTime": f'{datetime.now(tz=ZoneInfo("America/Edmonton")).replace(microsecond=0,second=0,minute=0,hour=18).isoformat()[:-6]}',
-        "timeZone": "Mountain Standard Time"
-    },
-               "availabilityViewInterval": "15"
-}
+def toggle_light(line):
+    with gpiod.request_lines(
+        "/dev/gpiochip0",
+        consumer="blink-example",
+        config={
+            line: gpiod.LineSettings(
+                direction=Direction.OUTPUT, output_value=Value.ACTIVE
+            )
+        },
+    ) as request:
+        status = request.get_value(line)
+        if status == Value.ACTIVE:
+            request.set_value(line, Value.INACTIVE)
+        else:
+            request.set_value(line, Value.ACTIVE)
 
-r = requests.post('https://graph.microsoft.com/v1.0/me/calendar/getschedule', data=cal_details)
-pprint.pprint(r.json())
-raise SystemExit
+@app.put("/v1/stoplight/red")
+async def red():
+    toggle_light(LINE_RED)
 
+@app.put("/v1/stoplight/yellow")
+async def yellow():
+    toggle_light(LINE_YLW)
 
-with gpiod.request_lines(
-    "/dev/gpiochip0",
-    consumer="blink-example",
-    config={
-        LINE_GRN: gpiod.LineSettings(
-            direction=Direction.OUTPUT, output_value=Value.ACTIVE
-        )
-    },
-) as request:
-    while True:
-        request.set_value(LINE_GRN, Value.ACTIVE)
-        time.sleep(1)
-        #request.set_value(LINE, Value.INACTIVE)
-        time.sleep(1)
+@app.put("/v1/stoplight/green")
+async def green():
+    toggle_light(LINE_GRN)
