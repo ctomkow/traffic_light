9f7eaeae5d635dff0627329cf337fac7665cc123
Craig Abt Tomkow <ctomkow@gmail.com>
2023-11-12 13:36:53 -0700

    threading

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,32 +1,16 @@
 
-@
-traffic_light.py,f\b\fb17d23ded3f311cc10b936b6b92eb89a8e7d2c9
 >
 pyproject.toml,5\d\5d07e7d72637aa0d59c89d381fe6dc4cf46e2491
 U
 %.github/workflows/release-package.yml,2\1\21464797e2a2877b680776108d9f930290302714
-L
-src/traffic_light/version.py,b\6\b65a07893d5cd3a42e0475bae410b2ba866bfa0b
-R
-"src/traffic_light/traffic_light.py,4\d\4d83643a9d0be05e380d83ed0b3f2391c2b7ce3f
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
-M
-src/traffic_light/__init__.py,e\b\eb030aae380cd5cff9f99631c90366b4c5385ee0
-T
-$src/ctomkow_traffic_light/version.py,e\a\ead5020d1eb7cfdcedf023470ff2b0a965659e64
-U
-%src/ctomkow_traffic_light/__init__.py,2\b\2b624716b2af1347e65a7b4d8e644ab0989ec107
-Z
-*src/ctomkow_traffic_light/traffic_light.py,e\f\efa977a044e940e534397a7464374e5d20df288f
 S
 #src/ctomkow_trafficlight/version.py,a\4\a4fb8eaa6625e7c9bc963666f5801f513a325c4e
 T
 $src/ctomkow_trafficlight/__init__.py,f\2\f2257104c272cc3a3f59858ab5da872c19fa188b
-Y
-)src/ctomkow_trafficlight/traffic_light.py,9\f\9f23102c8651453f8d329305af9b535120a4521b
 O
 src/ctomkow_trafficlight/app.py,0\2\0254080ad7a0d5c5cd35e0dd91680008b07c7157
--- src/ctomkow_trafficlight/app.py
+++ src/ctomkow_trafficlight/app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from threading import Thread
+from time import sleep
+
 import gpiod
 from gpiod.line import Direction, Value
 from flask import Flask, Response
@@ -7,40 +10,60 @@ from flask import Flask, Response
 LINE_RED = 9
 LINE_YLW = 10
 LINE_GRN = 11
+current_line = 11
+global_toggle_count = 0
 
 app = Flask(__name__)
 
-def toggle_light(line):
+def toggle_light():
+    toggle_count = global_toggle_count
     with gpiod.request_lines(
         "/dev/gpiochip0",
         consumer="blink-example",
         config={
-            line: gpiod.LineSettings(
+            current_line: gpiod.LineSettings(
                 direction=Direction.OUTPUT, output_value=Value.ACTIVE
             )
         },
     ) as request:
-        status = request.get_value(line)
-        if status == Value.ACTIVE:
-            request.set_value(line, Value.INACTIVE)
-        else:
-            request.set_value(line, Value.ACTIVE)
+        while True:
+            if toggle_count != global_toggle_count:
+                toggle_count = global_toggle_count
+                line_status = request.get_value(current_line)
+                if line_status == Value.ACTIVE:
+                    request.set_value(current_line, Value.INACTIVE)
+                else:
+                    request.set_value(current_line, Value.ACTIVE)
+                sleep(0.1)
+                continue
+            sleep(0.1)
 
 @app.route("/v1/trafficlight/red")
 def red():
-    toggle_light(LINE_RED)
+    global current_line
+    global global_toggle_count
+    current_line = LINE_RED
+    global_toggle_count += 1
     return Response(status=204)
 
 @app.route("/v1/trafficlight/yellow")
 def yellow():
-    toggle_light(LINE_YLW)
+    global current_line
+    global global_toggle_count
+    current_line = LINE_YLW
+    global_toggle_count += 1
     return Response(status=204)
 
 @app.route("/v1/trafficlight/green")
 def green():
-    toggle_light(LINE_GRN)
+    global current_line
+    global global_toggle_count
+    current_line = LINE_GRN
+    global_toggle_count += 1
     return Response(status=204)
 
 if __name__ == "__main__":
+    traffic_light_thread = Thread(target=toggle_light)
+    traffic_light_thread.start()
     app.run(host='0.0.0.0', port=5000)
 
